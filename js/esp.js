04ac23753055aa99d85575ff12b770d01b5d11c8
André Sünnemann <a.suennemann@edv-peuker.de>
2016-12-05 22:23:24 +0100

    added data-attributes for year, month and day to calendar integration

--- js/esp.js
+++ js/esp.js
@@ -461,11 +461,11 @@ esp =
                         // set class for today
                         if((year == currentYear) && (month == currentMonth) && (dayCounter == currentDay))
                         {
-                            tbody = tbody + '<td class="esp-calendar-today">' + dayCounter + '</td>';
+                            tbody = tbody + '<td class="esp-calendar-today" data-day="' + dayCounter + '" data-month="' + (month + 1) + '" data-year="' + year + '">' + dayCounter + '</td>';
                         }
                         else
                         {
-                            tbody = tbody + '<td>' + dayCounter + '</td>';
+                            tbody = tbody + '<td data-day="' + dayCounter + '" data-month="' + (month + 1) + '" data-year="' + year + '">' + dayCounter + '</td>';
                         }
                         dayCounter += 1;
                     }
